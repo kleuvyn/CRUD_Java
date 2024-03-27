d38258463cc6fea30fe64182cfc0eb2405b1cbb9
kleuvyn <kleuvyn@hotmail.com>
2024-03-26 21:55:07 -0300

    Criacao do projeto utilizando Spring initializer.

---
+++ src/main/java/br/dev/kleuvyn/pessoajava/PessoaJavaApplication.java
@@ -0,0 +1,13 @@
+package br.dev.kleuvyn.pessoajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoaJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/kleuvyn/pessoajava/PessoaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.kleuvyn.pessoajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
